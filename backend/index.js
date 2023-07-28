c82c1c9af2ad1ae00b0d65fc7f75c43860598ee4
MRGC0007 <info.mrgc07@gmail.com>
2023-07-28 17:41:45 +0530

    'verce535l.json'

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(
-{
-  origin: ['https://resume-6j37.vercel.app/'],
-  methods: ["POST","GET"],
-  credentials:true
-}
-
-));
+app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res)=>{
